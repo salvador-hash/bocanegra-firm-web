fbe0070ba99a0bc6344af2632c8715eab949e152
bocanegrafunds <bocanegrafunds@gmail.com>
2025-12-24 18:52:35 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-start gap-8 mb-12">
           <div>
             <span className="text-sm tracking-widest uppercase text-foreground">
-              Bocanegra Asset Management
+              Bocanegra Asset Management LP
             </span>
           </div>
           <div>
